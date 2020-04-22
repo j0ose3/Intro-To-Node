90465029ef59c6ff4423195ff81a9c559a42169c
Jose Angeles <humblekgames@gmail.com>
2020-04-22 01:42:19 -0400

    Added fs package to copy a file

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // jshint esversion:6
 const superheroes = require("superheroes");
 const supervillains = require('supervillains');
+const fs = require('fs');
+
+// fs.copyFileSync('file1.txt', 'file2.txt');
 
 var mySuperheroName = superheroes.random();
 var myVillainName = supervillains.random();
